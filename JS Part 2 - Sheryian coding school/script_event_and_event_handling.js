f4370c3b9d250100596d13877d11313538b1931e
Krs-Krish-108 <srivastavapriyanshu835@gmail.com>
2026-09-24 20:26:50 +0530

    commit for better organising the folder

--- JS Part 2 - Sheryian coding school/script_event_and_event_handling.js
+++ JS Part 2 - Sheryian coding school/script_event_and_event_handling.js
@@ -20,20 +20,63 @@
 //     console.log(e);
 // });
 //=----------------------------------------------------------------------------------
-let select = document.querySelector("select");
-let device = document.querySelector("#device");
+// let select = document.querySelector("select");
+// let device = document.querySelector("#device");
 
-select.addEventListener("change", function(e){
+// select.addEventListener("change", function(e){
     // console.log(e.target.value);
     // device.textContent=e.target.value;
     // device.textContent = "Device Selected";
-    device.textContent = `${e.target.value} Device Selected`;
-});
+//     device.textContent = `${e.target.value} Device Selected`;
+// });
 
 
 //=---------------------------------------------------------------------------
-//. Displaying every key being pressed on the main screen as a background image that can't be selected ---------> Project 1
+//. Creating a small display and dropdown to select device to select and selected device will be visbile on the display -------> Project 1
 
 
 //=---------------------------------------------------------------------------
-//. Creating a customized upload button and making it functional to choose files when clicked -----------------> Project 2
+//. Displaying every key being pressed on the main screen as a background image that can't be selected ---------> Project 2
+
+
+//=---------------------------------------------------------------------------
+//. Creating a customized upload button and making it functional to choose files when clicked -----------------> Project 3
+
+
+//=---------------------------------------------------------------------------
+//. Creating a Dashboard where you can fill a form and after submition, it will create a card of your filled details ----------> Project 4
+
+
+//=---------------------------------------------------------------------------
+//: Mouse over
+
+// let abcd1 = document.querySelector("#abcd1");
+
+// abcd1.addEventListener("mouseover", function(dets){
+//     abcd1.style.backgroundColor="yellow";
+// });
+
+// abcd1.addEventListener("mouseout", function(dets){
+//     abcd1.style.backgroundColor="red";
+// });
+
+
+
+
+
+// let abcd1=document.querySelector("#abcd1");
+
+// window.addEventListener("mousemove", (dets)=>{
+//     abcd1.style.left=dets.clientX + "px";
+//     abcd1.style.top=dets.clientY + "px";
+// });
+//=---------------------------------------------------------------------------
+//: Event bubbling
+//. Create a program that adds and removes a line though over any point of a list when click simultaniously 
+// let ulli = document.querySelector(".ulli");
+
+// ulli.addEventListener("click", (dets)=>{
+//     dets.target.classList.toggle("lt");    
+// });
+
+//=---------------------------------------------------------------------------
