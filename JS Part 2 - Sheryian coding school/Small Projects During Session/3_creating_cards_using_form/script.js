65df116d039bb3000fafc149a7597d5e0a0655de
Krs-Krish-108 <srivastavapriyanshu835@gmail.com>
2026-09-24 09:19:48 +0530

    just a regular commit to update my learnings

--- JS Part 2 - Sheryian coding school/Small Projects During Session/3_creating_cards_using_form/script.js
+++ JS Part 2 - Sheryian coding school/Small Projects During Session/3_creating_cards_using_form/script.js
@@ -1,1 +1,62 @@
-let
+let form = document.querySelector("form");
+let inp = document.querySelectorAll("input");
+let description = document.querySelector("textarea");
+let container = document.querySelector(".content");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+    console.log(inp[0].value);
+    console.log(description.value);
+    
+    
+    let card = document.createElement("div");
+    card.classList.add("card1");
+
+    let upper_half = document.createElement("div");
+    upper_half.classList.add("upper-half");
+
+    let profile = document.createElement("div");
+    profile.classList.add("profile");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", inp[0].value);
+
+
+    let hrl = document.createElement("hr");
+    hrl.classList.add("hrl");
+
+    let lower_half = document.createElement("div");
+    lower_half.classList.add("lower-half");
+
+    let h3 = document.createElement("h3");
+    h3.classList.add("name");
+    h3.textContent = inp[1].value;
+
+    let h4 = document.createElement("h4");
+    h4.classList.add("occupation");
+    h4.textContent = inp[2].value;
+
+    let p = document.createElement("p");
+    p.classList.add("description");
+    p.textContent = description.value;
+
+    profile.appendChild(img);
+    upper_half.appendChild(profile);
+
+    lower_half.appendChild(h3);
+    lower_half.appendChild(h4);
+    lower_half.appendChild(p);
+
+    card.appendChild(upper_half);
+    card.appendChild(hrl);
+    card.appendChild(lower_half);
+
+    container.appendChild(card);
+    
+    inp.forEach((input)=>{
+        if (input.type !== "submit"){
+            inp.value = "";
+        }
+    });
+})
